e79bb14206da519878495542e83dcd8f3e8b1126
user <user@DESKTOP-RO4PFG6>
2025-02-13 19:25:10 +0900

    안녕...

--- LifeOn/src/main/java/com/sp/app/lounge/controller/FreeBoardController.java
+++ LifeOn/src/main/java/com/sp/app/lounge/controller/FreeBoardController.java
@@ -5,10 +5,12 @@ import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -138,5 +140,60 @@ public class FreeBoardController {
 		}
 		
 		return "redirect:/lounge2/tip";
+	}
+	
+	@GetMapping("article/{psnum}")
+	public String article(
+			@PathVariable("psnum") long num,
+			@RequestParam(name = "page") String page,
+			@RequestParam(name = "schType", defaultValue = "all") String schType,
+			@RequestParam(name = "kwd", defaultValue = "") String kwd,
+			Model model,
+			HttpSession session) throws Exception {
+		
+		String query = "page=" + page;
+		
+		try {
+			kwd = URLDecoder.decode(kwd, "utf-8");
+			if (! kwd.isBlank()) {
+				query += "&schType=" + schType + "&kwd=" + URLEncoder.encode(kwd, "utf-8");
+			}
+			
+			service.updateHitCount(num);
+			
+			FreeBoard dto = Objects.requireNonNull(service.findById(num));
+			
+			// 엔터를 <Br>로 // 스마트에디터 사용 중
+			// dto.setContent(myUtil.htmlSymbols(dto.getContent()));
+			
+			Map<String, Object> map = new HashMap<>();
+			map.put("schType", schType);
+			map.put("kwd", kwd);
+			map.put("psnum", num);
+			
+			FreeBoard prevDto = service.findByPrev(map);
+			FreeBoard nextDto = service.findByNext(map);
+			
+			SessionInfo info = (SessionInfo) session.getAttribute("member");
+			map.put("nickname", info.getNickName());
+			boolean ismemberLiked = service.isMemberBoardLiked(map);
+			
+			model.addAttribute("dto", dto);
+			model.addAttribute("prevDto", prevDto);
+			model.addAttribute("nextDto", nextDto);
+			
+			model.addAttribute("ismemberLiked", ismemberLiked);
+
+			model.addAttribute("query", query);
+			model.addAttribute("page", page);
+			
+			return "lounge2/tip/article";
+			
+		} catch (NullPointerException e) {
+		} catch (Exception e) {
+			log.info("article : ", e);
+		}
+		
+		return "redirect:/lounge2/tip?" + query;
 	}
 }
