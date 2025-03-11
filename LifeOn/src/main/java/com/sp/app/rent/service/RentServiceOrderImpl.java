f99e958597eaa2382027bec71c0e93b41b6bb4ec
USER <USER@HelloWorld>
2025-03-11 18:08:00 +0900

    주요구현부분 코드주석

--- LifeOn/src/main/java/com/sp/app/rent/service/RentServiceOrderImpl.java
+++ LifeOn/src/main/java/com/sp/app/rent/service/RentServiceOrderImpl.java
@@ -18,34 +18,34 @@ public class RentServiceOrderImpl implements RentServiceOrder{
 	private final RentOrderMapper mapper;
 	
 	@Override
-	public String insertRentProductOrder(RentProductOrder dto) throws Exception {
+	public String insertRentProductOrder(RentProductOrder dto) throws Exception { // 물품 대여 신청
 		try {
-			int myPoint = mapper.memberPoint(dto.getNum());
+			int myPoint = mapper.memberPoint(dto.getNum()); // 구매자 잔여 포인트 조회
 			
-			if (myPoint >= dto.getOp()) {
-				dto.setPrep(-dto.getOp());
-				dto.setPretp(myPoint - dto.getOp());
+			if (myPoint >= dto.getOp()) { // 총 결제 금액보다 보유 포인트가 많으면
+				dto.setPrep(-dto.getOp()); // 결제 금액 세팅
+				dto.setPretp(myPoint - dto.getOp()); // 총 잔여 포인트 세팅
 				
-				mapper.insertPoint(dto);
-				mapper.insertRentProductOrder(dto);
-				mapper.updateStatus(dto);
+				mapper.insertPoint(dto); // 포인트 테이블에 결제 금액 데이터 추가
+				mapper.insertRentProductOrder(dto); // 주문 테이블에 주문내역 추가
+				mapper.updateStatus(dto); // 물품대여 게시글 상태 업데이트(대여가능 -> 대여중)
 				
-				dto.setSellerNum(mapper.sellerNum(dto.getPnum()));
-				myPoint = mapper.memberPoint(dto.getSellerNum());
-				dto.setPrep(dto.getOdp() * dto.getOdq());
-				dto.setPretp(myPoint + dto.getPrep());
-				mapper.sellerInsertPoint(dto);
+				dto.setSellerNum(mapper.sellerNum(dto.getPnum())); // 판매자 회원번호 조회
+				myPoint = mapper.memberPoint(dto.getSellerNum()); // 판매자 잔여 포인트 조회
+				dto.setPrep(dto.getOdp() * dto.getOdq()); // 판매 금액 세팅
+				dto.setPretp(myPoint + dto.getPrep()); // 총 잔여 포인트 세팅
+				mapper.sellerInsertPoint(dto); // 판매자에게 구매자가 결제한 포인트 입금
 				
 			} else {
-				return "noPoint";
+				return "noPoint"; // 잔여 포인트가 결제 금액보다 적을 경우 예외처리
 			}
 			
 		} catch (Exception e) {
-			log.info("insertRentProductOrder : ", e);
-			return "false";
+			log.info("insertRentProductOrder : ", e); // 예외 로그 노출
+			return "false"; // 실패처리
 		}
 		
-		return "true";
+		return "true"; // 성공처리
 	}
 	
 	@Override
